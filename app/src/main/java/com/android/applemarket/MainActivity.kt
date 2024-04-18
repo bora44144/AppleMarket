cb574bdf50aa43f76902ae2cc57bba25b7b5ceba
bora <bora44144@gmail.com>
2024-04-18 23:04:47 +0900

    Feat: 알림 기능 추가

--- app/src/main/java/com/android/applemarket/MainActivity.kt
+++ app/src/main/java/com/android/applemarket/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.android.applemarket
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.applemarket.databinding.ActivityMainBinding
 
@@ -54,8 +65,54 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
+        // BackButton 누르면 다이얼로그 띄우기
         this.onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
 
+        binding.notificationButton.setOnClickListener {
+            notification()
+        }
+    }
+
+    fun notification() {
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder: NotificationCompat.Builder
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // 26 버전 이상
+            val channelId = "one-channel"
+            val channelName = "My Channel One"
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                // 채널에 다양한 정보 설정
+                description = "My Channel One Description"
+                setShowBadge(true)
+                val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                val audioAttributes = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+                setSound(uri, audioAttributes)
+                enableVibration(true)
+            }
+            // 채널을 NotificationManager에 등록
+            manager.createNotificationChannel(channel)
+
+            // 채널을 이용하여 builder 생성
+            builder = NotificationCompat.Builder(this, channelId)
+        } else {
+            // 26 버전 이하
+            builder = NotificationCompat.Builder(this)
+        }
+        // 알림의 기본 정보
+        builder.run {
+            setSmallIcon(R.drawable.icon_circle_24)
+            setWhen(System.currentTimeMillis())
+            setContentTitle("키워드 알림")
+            setContentText("설정한 키워드에 대한 알림이 도착했습니다!!")
+        }
+        manager.notify(11, builder.build())
     }
 
 }
