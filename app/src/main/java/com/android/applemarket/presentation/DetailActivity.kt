fcc324693e0760afa6be53f1df05853e00ac1461
bora <bora44144@gmail.com>
2024-04-19 09:07:37 +0900

    Feat: 상품 상세페이지

---
+++ app/src/main/java/com/android/applemarket/presentation/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.android.applemarket.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.applemarket.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
